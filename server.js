1a0812b9c2cd119d902c41ad618810ec8c772e03
Phill Barron <phillipbarron242@gmail.com>
2020-08-27 19:35:11 +0100

    formatting tweak & sql cmd tweak

--- server.js
+++ server.js
@@ -10,29 +10,24 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-  if(err) throw err;
+  if (err) throw err;
   console.log("Connected to database!");
 });
 
 const wss = new WebSocket.Server({port: 8080})
 wss.on('connection', ws => {
   ws.on('message', message => {
-  
-    var jsonData = JSON.parse(message);
+    const jsonData = JSON.parse(message);
     const { appId: profileId } = jsonData;
     console.log(`Connection from ${profileId}`);
     
     if("timeOnPageMs" in jsonData) {
-    
       const { timeOn, page } = jsonData;
-
-      var sql = "UPDATE `analytics` SET `time_active` = time_active + " + timeOn + " WHERE `userId` = '" + profileId + "' L$
+      var sql = "UPDATE `analytics` SET `time_active` = time_active + " + timeOn + " WHERE `userId` = '" + profileId + '"';
       con.query(sql, function(err, result, fields) {
         if (err) throw err;
         console.log(`${profileId} logged ${(timeOn / 1000)} seconds on page ${page}`);
       });
-      
     }
-    
   });
 });
