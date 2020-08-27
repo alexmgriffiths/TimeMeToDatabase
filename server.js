61f8ce5e02254ccb894118006dae9112be58dbb8
Alex Griffiths <axgriffiths67@gmail.com>
2020-08-27 15:02:26 -0500

    Using pooling to make production ready

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
 const WebSocket = require('ws');
-var mysql = require('mysql');
+const mysql = require('mysql');
 require('dotenv').config();
 
 
 const { DB_HOST: host,  DB_USER: user, DB_PASS: password, DB_NAME: database } = process.env;
-
-const connection = mysql.createConnection({ host, user, password, database });
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to database!");
-});
+const pool = mysql.createPool({ host, user, password, database });
 
 const wss = new WebSocket.Server({port: 8080})
 wss.on('connection', ws => {
   ws.on('message', message => {
+
     const jsonData = JSON.parse(message);
-    const { appId: profileId } = jsonData;
-    console.log(`Connection from ${profileId}`);
-    
+    const { appId: timerId } = jsonData;
+
     if("timeOnPageMs" in jsonData) {
-      const { timeOn, page } = jsonData;
-      var sql = "UPDATE `analytics` SET `time_active` = time_active + " + timeOn + " WHERE `userId` = '" + profileId + '"';
-      connection.query(sql, function(err, result, fields) {
-        if (err) throw err;
-        console.log(`${profileId} logged ${(timeOn / 1000)} seconds on page ${page}`);
+
+      const { timeOnPageMs, pageName } = jsonData;
+      const sql = "UPDATE `mw_staff_timers` SET `time_active` = time_active + " + timeOnPageMs + " WHERE `id` =  " + timerId;
+
+      pool.getConnection((err, connection) => {
+        connection.query(sql, function(err, result, fields) {
+
+          connection.release();
+          if (err) throw err;
+
+        });
       });
     }
   });
 });
-setInterval(function() {
-  connection.query("SELECT 1");
-}, 5000);
